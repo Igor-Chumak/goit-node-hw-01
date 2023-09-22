9749045bebb414d206a8247358083cc3cd1c3af7
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-09-23 00:50:11 +0300

    to mentor

--- index.js
+++ index.js
@@ -35,17 +35,3 @@ program.parse();
 const options = program.opts();
 // console.log("options :>> ", options);
 invokeAction(options);
-
-// # Получаем и выводим весь список контактов в виде таблицы (console.table)
-// invokeActions({ action: "list" });
-// node index.js --action list
-// # Получаем контакт по id - выводим в консоль объект контакта или null, если контакта с таким id не существует.
-// invokeActions({ action: "get", id: "05olLMgyVQdWRwgKfg5J6" });
-// node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
-// # Добавляем контакт и выводим в консоль созданный контакт
-// invokeActions({ action: "add1", name: "Mango", email: "mango@gmail.com", phone: "22-22-22" });
-// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
-// # Удаляем контакт и выводим в консоль удаленный контакт или null, если контакта с таким id не существует.
-// invokeActions({ action: "remove", id: "qdggE76Jtbfd9eWJHrssH" });
-// node index.js --action remove --id qdggE76Jtbfd9eWJHrssH
-// invokeActions({ action: "list" });
